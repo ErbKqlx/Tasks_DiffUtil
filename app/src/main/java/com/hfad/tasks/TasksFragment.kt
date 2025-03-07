cb01e2cbde6c7e5bc305800a53f9e3a3c5d2a090
ErbKqlx <Error5455@yandex.ru>
2025-03-07 10:27:51 +0500

    Чтение записей

--- app/src/main/java/com/hfad/tasks/TasksFragment.kt
+++ app/src/main/java/com/hfad/tasks/TasksFragment.kt
@@ -24,6 +24,7 @@ class TasksFragment : Fragment() {
         val viewModel = ViewModelProvider(
                 this, viewModelFactory).get(TasksViewModel::class.java)
         binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
 
         return view
     }
